d5e8a1037ecac876bb58245967bf301f3e93b6ad
EricFedrowisch <efedrowisch@yahoo.com>
2024-07-18 22:39:22 -0500

    Shorten import paths
    Make imports less verbose by moving them into the __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
 # Package
+from .ai_chat_adapter import *
+
+__all__ = [
+    'AIChatAdapter',
+    'OpenAIWrapper',
+    'AnthropicWrapper',
+    'LocalWrapper',
+    'OllamaWrapper',
+    'ChatBuffer'
+    ]
